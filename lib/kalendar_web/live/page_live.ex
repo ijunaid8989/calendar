a8e405fd5f4af05e71f436139c04d7c8b6482168
Junaid Farooq <junaid@evercam.io>
2020-05-08 12:28:59 +0500

    Start Navigating month. && December Bug.

--- lib/kalendar_web/live/page_live.ex
+++ lib/kalendar_web/live/page_live.ex
@@ -3,6 +3,18 @@ defmodule KalendarWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, content: %{name: "Junaid"})}
+    {:ok, assign(socket, :kalendar, kalendar_state())}
+  end
+
+  def kalendar_state(date \\ Calendar.DateTime.now_utc) do
+    %{day: day, month: month, year: year} = date
+    %{
+      month: Timex.month_name(month),
+      year: year,
+      day: day,
+      month_number: month,
+      days_in_month: Timex.days_in_month(year, month),
+      days_in_next_month: Timex.days_in_month(year, (month + 1))
+    } |> IO.inspect
   end
 end
