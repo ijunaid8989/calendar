810bc05827b647d9db50ff4ef0d9d9cef12ea902
Junaid Farooq <junaid@evercam.io>
2020-05-10 13:13:33 +0500

    Handle December & year bug && Added prev_month handle_event.

--- lib/kalendar_web/live/components/side_kalendar.ex
+++ lib/kalendar_web/live/components/side_kalendar.ex
@@ -2,11 +2,21 @@ defmodule SideKalendar do
   use Phoenix.LiveComponent
   import String
 
-  def handle_event("next_month", %{"year" => year, "month" => month, "day" => day} = params, socket) do
+  def handle_event("next_month", %{"year" => year, "month" => month, "day" => day, "days_in_next_month" => days_in_next_month} = params, socket) do
     state =
       Calendar.DateTime.from_erl!({{to_integer(year), to_integer(month), to_integer(day)}, {0, 0, 0}}, "UTC", 123456)
-      |> Calendar.DateTime.add!(86400 * Timex.days_in_month(to_integer(year), (to_integer(month) + 1)))
+      |> Calendar.DateTime.add!(86400 * to_integer(days_in_next_month))
+      |> KalendarWeb.PageLive.kalendar_state
+    {:noreply, assign(socket, kalendar: state)}
+  end
+
+  def handle_event("prev_month", %{"year" => year, "month" => month, "day" => day, "days_in_prev_month" => days_in_prev_month} = params, socket) do
+    state =
+      Calendar.DateTime.from_erl!({{to_integer(year), to_integer(month), to_integer(day)}, {0, 0, 0}}, "UTC", 123456)
+      |> Calendar.DateTime.subtract!(86400 * to_integer(days_in_prev_month))
       |> KalendarWeb.PageLive.kalendar_state
     {:noreply, assign(socket, kalendar: state)}
   end
 end
+
+
